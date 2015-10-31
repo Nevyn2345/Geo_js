9484f26de14448f335fa58712b68cc5a85ca14c3
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2015-10-31 19:19:24 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function initialise() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var x = canvas.width;
+    var y = canvas.height;
+    var img = new Image();
+    img.src = 'medium_map.jpg';
+    canvas.addEventListener("mousedown", getcoords, false);
+
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0);
+    }
+
+    mainLoop();
+}
+
+function nextCity() {
+    alert("Your Next City is Berlin");
+}
+
+function getcoords(event) {
+    clickX = event.pageX;
+    clickY = event.pageY;
+    alert("X="+clickX +"y=" + clickY);
+}
+
+function mainLoop() {
+    nextCity();
+}
+
