5b8c06ff9d9ab09b804fb876377a61f192c789cc
Nevyn2345 <sbarnett1@sheffield.ac.uk>
2015-11-28 23:04:23 +0000

    Game now properly OOP, runs continously and crosshairs no longer stay after the go

--- dj/mygame/geogame/static/geogame/main.js
+++ dj/mygame/geogame/static/geogame/main.js
@@ -1,60 +1,81 @@
-function initialise() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var x = canvas.width;
-    var y = canvas.height;
-    var img = new Image();
-    img.src = '/static/geogame/medium_map.jpg';
-    canvas.addEventListener("mousedown", getcoords, false);
 
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0);
-    }
 
-    mainLoop();
-}
+Game = {};
 
-function nextCity() {
-    document.getElementById("target").innerHTML = "Your Next City is " + city.name + ", " + city.country;
-}
+Game.Launch = function() {
+    Game.ready = 0;
+    Game.Init = function() {
+        Game.ready = 1;
+        Game.canvas = document.getElementById("myCanvas");
+        Game.ctx = Game.canvas.getContext("2d");
+        Game.world = new Image();
+        Game.world.src = '/static/geogame/medium_map.jpg';
+        Game.citycross = new Image();
+        Game.citycross.src = '/static/geogame/targetcrosshair.gif';
+        Game.crosswidth = 50;
+        Game.crossheight = 50;
+        Game.canvas.addEventListener("mousedown", Game.getcoords, false);
 
-function getcoords(event) {
-    clickX = event.pageX;
-    clickY = event.pageY;
-    cityX = city.xcoord/10
-    cityY = city.ycoord/10
-    console.log("clickX " + clickX + " cityX " + cityX + " clickY " + clickY + " cityY " + cityY)
-    distance = Math.sqrt(Math.pow( clickX - cityX, 2) + Math.pow(clickY - cityY, 2));
-    if (distance < 50) {
-        alert("congratz");
-    } else {
-        alert("you suck!");
-    }
-    
-    //$.ajax('next-city/', function(data){
-    //        city = data;    
-    //        alert(city.name);
-    //    });
-
-    $.ajax({
-        url: 'next_city/',
-        type: 'get',
-        success: function(data) {
-            city = data;
-            nextCity();
-        },
-        failure: function(data) {
-            console.log("error");
-            alert('error!');
+        Game.world.onload = function() {
+            Game.ctx.drawImage(Game.world, 0, 0);
         }
-    });
+
+        Game.mainloop();
+    }
+
+    Game.getcoords = function(event) { //get's click location on the canvas
+        Game.clickX = event.PageX;
+        Game.clickY = event.PageY;
+        Game.ctx.drawImage(Game.world, 0, 0);
+        Game.distance_calc();
+    }
+
+    Game.distance_calc = function() {
+        var cityX = Game.city.xcoord/10;
+        var cityY = Game.city.ycoord/10;
+        distance = Math.sqrt(Math.pow( Game.clickX - cityX, 2) + Math.pow(Game.clickY - cityY, 2));
+
+        if (distance < 50) {
+            alert("congratz");
+        } else {
+            alert("learn geography fool!");
+        }
+
+        Game.ctx.drawImage(Game.citycross, cityX - Game.crosswidth/2, cityY - Game.crossheight/2);
+        Game.getCity();
+    }
+
+    Game.getCity = function() { // call's views.py to get a new city object
+        $.ajax({
+            url: 'next_city/',
+            type: 'get',
+            success: function(data) {
+                Game.city = data;
+                Game.nextCity();
+            },
+            failure: function(data) {
+                console.log("error");
+                alert('error!');
+            }
+        });
+    }
+
+    Game.nextCity = function() { // updates target location in HTML
+        document.getElementById("target").innerHTML = "Your next city is " + Game.city.name + ", " + Game.city.country;
+    }
+
+    Game.mainloop = function() {
+       Game.getCity();
+    }
+
 }
 
-function mainLoop() {
-    nextCity();
+/*=============================================
+Let's Go
+=============================================*/
 
-}
+Game.Launch()
 
-window.onload=function() {
-    initialise()
+window.onload = function() {
+    Game.Init();
 }
